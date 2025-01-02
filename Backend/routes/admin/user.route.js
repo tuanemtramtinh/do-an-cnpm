1ff361cf321815c63396648d851b1fcb49766a35
xuanloc5008 <nxloc.2004@gmail.com>
2025-01-02 22:32:14 +0700

    small update

--- Backend/routes/admin/user.route.js
+++ Backend/routes/admin/user.route.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../../controllers/admin/user.controller");
-// const { requireAuth } = require("../../middlewares/auth.middleware");
-// const { isAdmin } = require("../../middlewares/role.middleware");
+const authMiddleware = require("../../middlewares/auth.middleware");
+const {isAdmin} = require("../../middlewares/isAdmin.middleware");
 
-router.post("/change-role/:id", controller.changeRole);
-router.get("/getAlluser", controller.getAllUsers);
-router.get("/getComment", controller.getAllComments);
+router.post("/change-role/:id", controller.changeRole, isAdmin.isAdmin);
+router.get("/getAlluser", controller.getAllUsers, authMiddleware.requireAuth);
+router.get("/getComment", controller.getAllComments, authMiddleware.requireAuth);
 
 module.exports = router;
